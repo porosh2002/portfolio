40d57c933117c3db185c58dc9a445c671c578c52
porosh2002 <jamilkashem2002@gmail.com>
2021-06-04 20:27:35 +0600

    Menu state update [ 1.0.1 ]

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ const CloseIcon = document.getElementById('CloseIcon')
 const OpenIcon = document.getElementById('OpenIcon')
 const LinksContainer = document.getElementById('LinksContainer')
 const Links = document.getElementById('Links')
+const About = document.getElementById('About')
+const Portfolio = document.getElementById('Portfolio')
+const Contact = document.getElementById('Contact')
 OpenIcon?.addEventListener('click',()=>{
     CloseIcon?.classList.remove('hidden')
     OpenIcon?.classList.add('hidden')
@@ -22,3 +25,10 @@ CloseIcon?.addEventListener('click',()=>{
 Links?.addEventListener('click',()=>{
     CloseMenu()
 })
+if(window.location.pathname === '/about'){
+    About?.classList.add('opacity-70') 
+}
+if(window.location.pathname === '/contact'){
+
+    Contact?.classList.add('bg-gray-400') 
+}
