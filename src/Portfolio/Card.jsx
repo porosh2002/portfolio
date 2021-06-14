a42f815bd450c65e46a0405e1866ce6c0a54ff9e
porosh2002 <jamilkashem2002@gmail.com>
2021-06-14 20:12:07 +0600

    Portfolio Card update 2.0

--- src/Portfolio/Card.jsx
+++ src/Portfolio/Card.jsx
@@ -5,7 +5,7 @@ const {Name,Delivery,Live,Image,GithubLink,Type} = data;
         <div className='h-48 w-full bg-gradient-to-r from-green-400 to-blue-500  rounded-sm'>
         <p className='h-full flex items-center justify-center tracking-wider text-xl text-white'>{Name}</p>
         </div>
-        <div className='text-gray-700 flex h-8 items-center justify-end bg-gray-300'>
+        <div className='text-gray-700 flex h-8 items-center justify-end'>
         <a href='#' className='mx-2'><svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M2 5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm3.293 1.293a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 01-1.414-1.414L7.586 10 5.293 7.707a1 1 0 010-1.414zM11 12a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd" /></svg></a>
         <a href='#' className='mx-2'><svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd" /></svg></a>
         
