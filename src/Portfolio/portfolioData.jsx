3ca869b6f6c6ccfa9383064eeddef6ede9c3c47a
porosh2002 <jamilkashem2002@gmail.com>
2021-06-27 18:45:41 +0600

    data updated

--- src/Portfolio/portfolioData.jsx
+++ src/Portfolio/portfolioData.jsx
@@ -1,39 +1,98 @@
 export const Data = [
 {
+    Name:'Digital signal processor',
+    Live:null,
+    Image:'https://www.dropbox.com/s/yffzx6d8vl5wrqg/alldsp.jpg?dl=0',
+    SourceCode:null,
+},
+{
+    Name:'cleaning',
+    Live:null,
+    Image:'https://www.dropbox.com/s/lwcvvxvfk637yly/sshC.jpg?dl=0',
+    SourceCode:null,
+},
+{
+    Name:'Bookmark',
+    Live:'https://bookmarkkeepar.netlify.app/',
+    Image:'https://www.dropbox.com/s/j782bl3olzz4ny3/bookmark.png?dl=0',
+    SourceCode:'https://github.com/porosh2002/Bookmark-Keeper',
+},
+{
+    Name:' connect',
+    Live:'https://connectdesign.netlify.app/',
+    Image:'https://www.dropbox.com/s/c9i8pp6jzqc8sm7/connect.jpg?dl=0',
+    SourceCode:'https://github.com/porosh2002/Connect-Personnel',
+},
+{
+    Name:'Manufacturing',
+    Live:null,
+    Image:'https://www.dropbox.com/s/jexdn292iuup2q3/hannah.png?dl=0',
+    SourceCode:null,
+},
+{
+    Name:'splash form',
+    Live:null,
+    Image:'https://www.dropbox.com/s/f0jp2s8ejtm9wun/vtn.png?dl=0',
+    SourceCode:null,
+},
+{
+    Name:'cannabis farm',
+    Live:'https://cradle-canna.netlify.app/',
+    Image:'https://www.dropbox.com/s/whiue4bk06hfrss/cradle-canna.jpg?dl=0',
+    SourceCode:null,
+},
+{
+    Name:'Mega menu',
+    Live:null,
+    Image:'https://www.dropbox.com/sh/evzanimrpzh9p78/AADWDSyj659_d94Lj4WDeYl8a?dl=0',
+    SourceCode:null,
+},
+{
+    Name:'crypto Token',
+    Live:null,
+    Image:'https://www.dropbox.com/s/p3na8sd7fdqysdo/token.jpg?dl=0',
+    SourceCode:null,
+},
+{
+    Name:'gaming community',
+    Live:null,
+    Image:'https://www.dropbox.com/s/koaa39a51v4xse5/ludustack.png?dl=0',
+    SourceCode:null,
+},
+{
+    Name:'Internet Service Provider',
+    Live:null,
+    Image:'https://www.dropbox.com/s/60mgfimoepasuc6/quattrocom.jpg?dl=0',
+    SourceCode:null,
+}
+,{
     Name:' warehousing',
     Live:'https://shirtee.netlify.app/',
     Image:'https://www.dropbox.com/s/hftzegw5cziynr2/shirtee.jpg?dl=0',
     SourceCode:'https://github.com/porosh2002/shirtee.cloud',
-    BG:"SC"
 }
 ,{
     Name:'Youtube Clone',
     Live:null,
     Image:'https://www.dropbox.com/sh/zxaye9lmakscg71/AADXbDv2jFPVFdsPk80UNYFEa?dl=0',
     SourceCode:'https://github.com/porosh2002/Youtube-Clone',
-    BG:"YC"
 }
 ,{
     Name:'Face detection',
     Live:'https://facedetectionporosh.netlify.app/',
     Image:'https://www.dropbox.com/s/icqdln9h4gw85wg/Face.png?dl=0',
     SourceCode:'https://github.com/porosh2002/Face_Detection',
-    BG:"FD"
 }
 ,{
     Name:'LINK OUTREACH',
-    Delivery:['DESIGN',' & ','CODE'],
     Live:'https://www.google.com',
     Image:'https://www.google.com',
     SourceCode:'https://www.google.com',
-    BG:"LO"
 }
 ,{
     Name:'Infrastructure',
-    Delivery:['DESIGN',' & ','CODE'],
-    Live:'https://www.google.com',
-    Image:'https://www.google.com',
-    SourceCode:'https://www.google.com',
-    BG:"INF"
+    Live:'https://bmmgroup.netlify.app/',
+    Image:'https://www.dropbox.com/s/p1zskurk2l8w9tg/bmmgroup.jpg?dl=0',
+    SourceCode:null,
 }
 ]
