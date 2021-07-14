7302c60d02b337f24f37bff32582067536c8fb50
porosh2002 <jamilkashem2002@gmail.com>
2021-07-15 03:05:20 +0600

    GITHUB ADDED

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
+import moment from 'moment'
 const CloseIcon = document.getElementById('CloseIcon')
 const OpenIcon = document.getElementById('OpenIcon')
 const LinksContainer = document.getElementById('LinksContainer')
 const Links = document.getElementById('Links')
 const About = document.getElementById('About')
-const Portfolio = document.getElementById('Portfolio')
 const Contact = document.getElementById('Contact')
 const ContactPage = document.getElementById('ContactPage')
 const ContactTimes = document.getElementById('ContactTimes')
 const ExpDays = document.getElementById('ExpDays')!
 const AboutPageDev = document.getElementById('AboutPageDev')
 const DevTimes = document.getElementById('AboutTimes')
-import moment from 'moment'
 const EXP = document.getElementById('Exp')!
+
+
 OpenIcon?.addEventListener('click', () => {
     CloseIcon?.classList.remove('hidden')
     OpenIcon?.classList.add('hidden')
@@ -32,63 +33,18 @@ CloseIcon?.addEventListener('click', () => {
 Links?.addEventListener('click', () => {
     CloseMenu()
 })
-var state = 'root'
-About?.addEventListener('click', () => {
-    state = 'about'
-    if (state === 'about') {
-        About?.classList.add('opacity-70')
-        Contact?.classList.remove('opacity-70')
-        Portfolio?.classList.remove('opacity-70')
-    }
-})
-Portfolio?.addEventListener('click', () => {
-    state = 'root'
-    if (state === 'root') {
-        About?.classList.remove('opacity-70')
-        Contact?.classList.remove('opacity-70')
-        Portfolio?.classList.add('opacity-70')
-    }
-})
 Contact?.addEventListener('click', () => {
-    state = 'contact'
-    if (state === 'contact') {
-        About?.classList.remove('opacity-70')
-        Contact?.classList.add('opacity-70')
-        Portfolio?.classList.remove('opacity-70')
-    }
     ContactPage?.classList.remove('-translate-y-full')
 })
 ContactTimes?.addEventListener('click', () => {
-
     ContactPage?.classList.add('-translate-y-full')
-    state = 'root'
-    if (state === 'root') {
-        About?.classList.remove('opacity-70')
-        Contact?.classList.remove('opacity-70')
-        Portfolio?.classList.add('opacity-70')
-    }
 })
 About?.addEventListener('click', () => {
-    state = 'about'
-    if (state === 'about') {
-        About?.classList.add('opacity-70')
-        Contact?.classList.remove('opacity-70')
-        Portfolio?.classList.remove('opacity-70')
-    }
     AboutPageDev?.classList.remove('-translate-y-full')
 })
 DevTimes?.addEventListener('click', () => {
     AboutPageDev?.classList.add('-translate-y-full')
-    state = 'root'
-    if (state === 'root') {
-        About?.classList.remove('opacity-70')
-        Contact?.classList.remove('opacity-70')
-        Portfolio?.classList.add('opacity-70')
-    }
 })
-if (state === 'root') {
-    Portfolio?.classList.add('opacity-70')
-}
 const EXPvalue = moment("20180301", "YYYYMMDD").fromNow(true);
 const d = new Date().getMonth();
 var Extramonth = 0;
